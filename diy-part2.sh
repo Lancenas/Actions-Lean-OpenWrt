0cba3c4c9bcaa047c5fe6b690c3e477bac82d0d5
Lancenas <32763979+Lancenas@users.noreply.github.com>
2021-08-15 09:22:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #============================================================
-# https://github.com/P3TERX/Actions-OpenWrt
+# https://github.com/Lancenas/Actions-Lean-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 # Lisence: MIT
