4668b548bbe067c2277f070133e7a204c591b233
xinpenghan <xinpenghan@users.noreply.github.com>
2023-05-26 21:35:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 #============================================================
 
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.1.111/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.111/g' package/base-files/files/bin/config_generate
 
 chmod -R 755 files
 chmod -R 755 package
